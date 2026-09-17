ceaad2e92511a2e877a2f7ca7585a5f14160764e
joel-nwamba <joel.elyon.nwamba@gmail.com>
2026-09-16 22:50:03 -0400

    able to fetch movie list when typing

--- js/index.js
+++ js/index.js
@@ -4,18 +4,32 @@ let apiky = "54b19a43";
 
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
+let movieList= document.getElementById("movie-list");
 
 searchInput.addEventListener("input", () => {
     const newInput = searchInput.value;
     if(newInput === "") {
         // clear movie 
-
+        movieList.innerHTML = " ";
     } else {
         fetch(`http://www.omdbapi.com/?s=${newInput}&apikey=54b19a43`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
+            if(data.Response === "True") {
+                movieList.innerHTML = " ";
+                for(const movieData of data.Search) {
+                       movieList.innerHTML +=  `
+                        <div class="movie">
+                            <img src="${movieData.Poster}" alt="${movieData.Title} poster">
+                            <h2>${movieData.Title}</h2>
+                        </div>
 
+                        `
+                }
+            } else {
+                 movieList.innerHTML = `<p>${data.Error}</p>`;
+            }
         })
     }
 })
