a316311abe8d7bec741cadf5d3d5f19e528a7313
joel-nwamba <joel.elyon.nwamba@gmail.com>
2026-09-17 21:20:03 -0400

    Added styling, next iss to work on having the button be functional

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ searchInput.addEventListener("input", () => {
         // clear movie 
         movieList.innerHTML = " ";
     } else {
-        fetch(`http://www.omdbapi.com/?s=${newInput}&apikey=54b19a43`)
+        fetch(`http://www.omdbapi.com/?s=${newInput}&plot=full&apikey=54b19a43`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
@@ -21,17 +21,23 @@ searchInput.addEventListener("input", () => {
                 for(const movieData of data.Search) {
                        movieList.innerHTML +=  `
                         <div class="movie">
-                            <img src="${movieData.Poster}" alt="${movieData.Title} poster">
+                            <img src="${movieData.Poster}" alt="${movieData.Title} poster" class="movie-img">
                             <h2>${movieData.Title}</h2>
+                            <h3>${movieData.Year}</h3>
+                            <h3>${movieData.Type}</h3>
                         </div>
 
                         `
                 }
             } else {
-                 movieList.innerHTML = `<p>${data.Error}</p>`;
+                 movieList.innerHTML = `<p>${data.Error} Unable to find what you're looking for? Please try another search</p>`;
             }
         })
     }
+});
+
+searchButton.addEventListener("click", () => {
+    
 })
 
 
